844dcfe99b52b07da2191e1bbae38929db57b759
Jacobsmi <jacobsm84@gmail.com>
2021-05-07 11:46:21 -0400

    Open Position Complete

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
+import { useEffect, useState } from 'react'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+
+  const [openPositions, setOpenPositions] = useState([])
+
+  useEffect(()=>{
+    console.log("This is a test")
+  },[])
   return (
     <div className={styles.container}>
         
--- pages/openposition.js
+++ pages/openposition.js
@@ -1,10 +1,18 @@
+import router from 'next/router'
 import styles from '../styles/OpenPosition.module.css'
+import Image from 'next/image'
+import { useState } from 'react'
 
 export default function AddTrade() {
 
-  function addNewTrade() {
+  const [errors, setErrors] = useState(false)
+
+  function verifyValues() {
     // track if entry is valid
     let validTrade = true
+
+    // create a string to track errors
+    let errorString = 'Errors<ul>'
 
     // verify values with regex
 
@@ -12,69 +20,120 @@ export default function AddTrade() {
     let symbol = document.getElementById('position-symbol').value.toUpperCase()
     if (!/^[A-Z]{4,5}$/.test(symbol)) {
       validTrade = false
-      console.log("Not a good symbol")
+      errorString += '<li>Invalid Symbol</li>'
     }
 
+    // verify quantity
     let quantity = document.getElementById('position-quantity').value
-    if (!/^\d+\.\d{0,10}$/.test(quantity)) {
+    if (!/^\d+\.{0,1}\d{0,10}$/.test(quantity)) {
       validTrade = false
-      console.log("Not a good quantity")
+      errorString += '<li>Invalid Quantity</li>'
     }
 
+    // verify cost
     let cost = document.getElementById('position-cost').value
-    if (!/^\d+\.\d{0,10}$/.test(cost)) {
+    if (!/^\d+\.{0,1}\d{0,10}$/.test(cost)) {
       validTrade = false
-      console.log("Not a good cost")
+      errorString += '<li>Invalid Cost</li>'
     }
 
-    // if everything is valid add it to the local storage list
+    // any date chosen by the picker is valid so just make sure one exists
+    let date = document.getElementById('position-date').value
+    if (date === '') {
+      validTrade = false
+      errorString += '<li>No Date Selected</li>'
+    }
+    // close error string, done with validation
+    errorString += '</ul>'
+
+    // set the value of error string
+    document.getElementById('position-errors').innerHTML = errorString
+
     if (validTrade) {
-      // get the current value from local storage
-      let openPositions = JSON.parse(localStorage.getItem('open-positions'))
+      // hide error div
+      setErrors(false)
+      return true
+    } else {
+      // show error div
+      setErrors(true)
+      return false
+    }
 
-      // if there is nothing currently in local storage create an array of objects and store it
-      if (openPositions === null) {
-        console.log("Adding to empty list")
-        openPositions = JSON.stringify([{
-          symbol: symbol,
-          quantity: document.getElementById('position-quantity').value,
-          cps: document.getElementById('position-cost').value,
-          date: document.getElementById('position-date').value
-        }])
-        localStorage.setItem('open-positions', openPositions)
+  }
 
-      } else {
-        // otherwise adds trade to existing list if there are already entries
-        openPositions.push({
-          symbol: document.getElementById('position-symbol').value,
-          quantity: document.getElementById('position-quantity').value,
-          cps: document.getElementById('position-cost').value,
-          date: document.getElementById('position-date').value
-        })
-        localStorage.setItem('open-positions', JSON.stringify(openPositions))
-      }
+
+  function storePosition() {
+
+    // get the current value from local storage
+    let openPositions = JSON.parse(localStorage.getItem('open-positions'))
+
+    // if there is nothing currently in local storage create an array of objects and store it
+    if (openPositions === null) {
+      console.log("Adding to empty list")
+      openPositions = JSON.stringify([{
+        symbol: document.getElementById('position-symbol').value,
+        quantity: document.getElementById('position-quantity').value,
+        cps: document.getElementById('position-cost').value,
+        date: document.getElementById('position-date').value
+      }])
+      localStorage.setItem('open-positions', openPositions)
+
+    } else {
+      // otherwise adds trade to existing list if there are already entries
+      openPositions.push({
+        symbol: document.getElementById('position-symbol').value,
+        quantity: document.getElementById('position-quantity').value,
+        cps: document.getElementById('position-cost').value,
+        date: document.getElementById('position-date').value
+      })
+      localStorage.setItem('open-positions', JSON.stringify(openPositions))
+    }
+    // redirect back to the home page
+    router.push('/')
+  }
+
+
+  function addNewTrade() {
+    // verifies values from form
+    let validTrade = verifyValues()
+
+    // if all values valid store the position
+    if (validTrade) {
+      storePosition()
     }
   }
 
   return (
     <div className={styles.container}>
-      <div className={styles.side}>
-
+      <div className={styles.top}>
+        <Image
+          src='/waves.svg'
+          layout='fill'
+          objectFit='cover'
+        />
       </div>
       <div className={styles.form}>
-        <h1>Add Position</h1>
-        <label for='position-symbol'>Symbol:</label>
+        <h2>Open a Position</h2>
+        <div
+          id='position-errors'
+          style={{
+            display: errors ? 'block' : 'none',
+            backgroundColor: '#EE6C4D',
+            padding: '10px',
+            borderRadius: '10px',
+            marginBottom: '20px'
+          }}
+        >
+        </div>
+        <label htmlFor='position-symbol'>Symbol</label>
         <input id='position-symbol' placeholder='Ticker Symbol' type='text'></input><br />
-        <label for='position-quantity'>Quantity:</label>
+        <label htmlFor='position-quantity'>Quantity</label>
         <input id='position-quantity' placeholder='Quantity' type='text'></input><br />
-        <label for='position-cost'>Cost per Share:</label>
+        <label htmlFor='position-cost'>Cost per Share</label>
         <input id='position-cost' placeholder='Cost per Share' type='text'></input><br />
-        <label for='position-date'>Date of trade:</label>
+        <label htmlFor='position-date'>Date of trade</label>
         <input id='position-date' placeholder='Date' type='date'></input><br />
         <button onClick={addNewTrade}>Add Position</button>
-      </div>
-      <div className={styles.side}>
-
       </div>
     </div>
   )
